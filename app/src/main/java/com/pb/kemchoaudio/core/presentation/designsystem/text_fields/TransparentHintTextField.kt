10b93b6dff0ee2cc7e5e71131ce8792e21f9b072
Pratik Bhandari <prateek.9770@gmail.com>
2026-05-18 16:34:48 +0530

    Reusable components

---
+++ app/src/main/java/com/pb/kemchoaudio/core/presentation/designsystem/text_fields/TransparentHintTextField.kt
@@ -0,0 +1,69 @@
+package com.pb.kemchoaudio.core.presentation.designsystem.text_fields
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import com.pb.kemchoaudio.core.presentation.designsystem.theme.KemChoAudioTheme
+
+@Composable
+fun TransparentHintTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    onValueChange: (String) -> Unit,
+    hintText: String? = null,
+    hintColor: Color = MaterialTheme.colorScheme.outlineVariant,
+    textStyle: TextStyle = MaterialTheme.typography.bodyMedium.copy(
+        color = MaterialTheme.colorScheme.onSurfaceVariant
+    ),
+    maxLines: Int = Int.MAX_VALUE,
+    singleLine: Boolean = false,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+) {
+    BasicTextField(
+        value = text,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        textStyle = textStyle,
+        maxLines = maxLines,
+        singleLine = singleLine,
+        keyboardActions = keyboardActions,
+        keyboardOptions = keyboardOptions,
+        decorationBox = { innerTextField ->
+            Box(
+                contentAlignment = Alignment.CenterStart
+            ) {
+                if (text.isBlank() && hintText != null) {
+                    Text(
+                        text = hintText,
+                        style = textStyle,
+                        color = hintColor
+                    )
+                } else {
+                    innerTextField()
+                }
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    KemChoAudioTheme {
+        TransparentHintTextField(
+            text = "",
+            onValueChange = {},
+            hintText = "Enter text here"
+        )
+    }
+}
